8baeaefc2b70041e21cffceba84792341102fea9
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2025-02-19 15:46:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ async function sendMainMenu(ctx) {
   }
 
   const messageText = `*Selamat datang di ${NAMA_STORE},
-Powered by FTVPN* 🚀
+Powered by RiswanJabar* 🚀
 Bot VPN serba otomatis untuk membeli
 layanan VPN dengan mudah dan cepat
 Nikmati kemudahan dan kecepatan
